e24ad38f18e7c3a3d690d282c242d11dca95b2ee
Zach Holland <zholland@google.com>
2022-07-19 15:51:18 -0600

    Adds ability to specify the agent, step size, epsilon, trace decay rate, and walls via the command line.

--- RLtoolkit/gridworld/gwDemoG.py
+++ RLtoolkit/gridworld/gwDemoG.py
@@ -18,11 +18,23 @@ The goal square is a terminal state.  Reward is +1 for reaching the goal, 0 else
 """
 
 ###
+import functools
 
 from .gwguimain import *
 from .gwobject import *
 import argparse
+import random
 
+AGENT_CLASSES = {
+    "Sarsa": SarsaGridAgent,
+    "SarsaLambda": SarsaLambdaGridAgent,
+    "Qlambda": QlambdaGridAgent,
+    "Qonestep": QonestepGridAgent,
+    "Dyna": DynaGridAgent,
+}
+
+
+# TRACE_AGENTS = set([QlambdaGridAgent, SarsaLambdaGridAgent])
 
 def runDemo():
   makeGridworldSimulation(16, 16, 87, 15, 30)
@@ -35,12 +47,37 @@ def runObjDemo(
     start_state=87,
     goal_state=15,
     square_size=30,
+    speed=0,
+    agent_class=DynaGridAgent,
+    alpha=0.5,
+    epsilon=0.05,
+    lambda_=0.8,
+    walls=None,
 ):
-  makeObjectGridworldSimulation(w=width,
-                                h=height,
-                                st=start_state,
-                                g=goal_state,
-                                size=square_size)
+  random.seed(42)
+
+  agent_class = functools.partial(agent_class, alpha=alpha, epsilon=epsilon,
+                                  agentlambda=lambda_)
+
+  window = makeObjectGridworldSimulation(w=width,
+                                         h=height,
+                                         st=start_state,
+                                         g=goal_state,
+                                         size=square_size,
+                                         agentclass=agent_class)
+  if walls:
+    for s in walls:
+      window.gridview.toggleBarrier(s)
+
+  if speed < 0:
+    for _ in range(0, speed, -1):
+      print(f'speed slow')
+      window.simSlower()
+  elif speed > 0:
+    for _ in range(speed):
+      print(f'speed fast')
+      window.simFaster()
+
   gMainloop()
 
 
@@ -57,10 +94,36 @@ def main():
   parser.add_argument("-sz", "--size",
                       help="The size of each grid cell in pixels.",
                       default=30, type=int)
+  parser.add_argument("-sp", "--speed",
+                      help=("The starting speed of the simulation relative "
+                            "to the default speed (default=0)."),
+                      default=0, type=int)
+  parser.add_argument("-a", "--agent",
+                      help=(f"The agent class to run. "
+                            f"Options are: {list(AGENT_CLASSES.keys())}."),
+                      default="Dyna", type=str)
+  parser.add_argument("--alpha", help="The agent step size.", default=0.5,
+                      type=float)
+  parser.add_argument("--epsilon",
+                      help=("The exploration probability for "
+                            "the epsilon-greedy policy."),
+                      default=0.05, type=float)
+  parser.add_argument("--trace_decay",
+                      help=("The eligibility trace decay rate. For agents "
+                            "without traces, this argument has no effect."),
+                      default=0.8, type=float)
+  parser.add_argument("--walls",
+                      nargs='+', default=[], type=int,
+                      help=("A list of locations to place barriers "
+                            "in the gridworld."))
   args = parser.parse_args()
 
+  print(args.walls)
+
   runObjDemo(width=args.width, height=args.height, start_state=args.start,
-             goal_state=args.goal, square_size=args.size)
+             goal_state=args.goal, square_size=args.size, speed=args.speed,
+             agent_class=AGENT_CLASSES[args.agent], alpha=args.alpha,
+             epsilon=args.epsilon, lambda_=args.trace_decay, walls=args.walls)
 
 
 if __name__ == '__main__':
